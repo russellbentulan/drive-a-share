59d6f474e1b4cab6ef9ba7e9d87631abba995388
Russell Bentulan <russellbentulan@gmail.com>
2019-10-12 22:02:40 -0400

    initiated first api call

--- scripts.js
+++ scripts.js
@@ -1,12 +1,31 @@
 // Namespace Object
-driveShare = {};
+dasApp = {};
+
+// Caching selectors
+dasApp.originInput = $('#origin-input');
+dasApp.originDatalist = $('#origin-locations');
+
+// Get location hints
+dasApp.searchLocation = searchTerms => {
+    $.ajax({
+        url: `${dasAppConfig.mbPlaces}${searchTerms}.json?access_token=${dasAppConfig.mbToken}&autocomplete=true&types=place&limit=5`,
+        method: 'GET',
+        dataType: 'json'
+    }).then(data => {
+        console.log(data);
+    });
+}
 
 // init Function
-driveShare.init = () => {
-    console.log(driveShareSecret)
+dasApp.init = () => {
+
+    dasApp.originInput.on('keyup', function() {
+        originSearchTerms = $(this).val();
+        dasApp.searchLocation(originSearchTerms);
+    });
 };
 
 // Document Ready
 $(function() {
-    driveShare.init();
-})
+    dasApp.init();
+});
