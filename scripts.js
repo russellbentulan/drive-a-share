997059acf458dbbc36a05aaca923dccb70e6f43f
Russell Bentulan <russellbentulan@gmail.com>
2019-10-13 10:56:56 -0400

    added logging object to save place names and their coordinates

--- scripts.js
+++ scripts.js
@@ -3,26 +3,28 @@ dasApp = {};
 
 // Caching selectors
 dasApp.originInput = $('#origin-input');
-dasApp.originDatalist = $('#origin-locations');
 dasApp.destinationInput = $('#destination-input');
-dasApp.destionationDatalist = $('#destination-locations')
 
+// Log searched cities with their coordinate array to save extra api requests
+dasApp.loggedLocations = {};
 
 // Get value of input element
-// Query data every 3rd change to input
+// Query data once 3 or more letters have been entered
 // Add options to the datalist using the response
+// Save location name and coordinate array into the log object
 dasApp.getSearchTerms = function() {
     searchTerms = $(this).val();
-    if ( searchTerms.length % 3 === 0 ) {
+    if ( searchTerms.length >= 3 ) {
         $.ajax({
             url: `${dasAppConfig.mbPlaces}${searchTerms}.json?access_token=${dasAppConfig.mbToken}&autocomplete=true&types=place&limit=5`,
             method: 'GET',
             dataType: 'json'
         }).then(data => {
             const allLocations = data.features.values();
-            for (const location of allLocations) {
-                const optiontHtml = `<option value="${location.place_name}"`;
+            for (const locationInfo of allLocations) {
+                const optiontHtml = `<option value="${locationInfo.place_name}"`;
                 $(this).next('datalist').append(optiontHtml);
+                dasApp.loggedLocation[locationInfo.place_name] = locationInfo.center;
             }
         }).catch(err => {
             console.log("There must have been a mistake", err);
