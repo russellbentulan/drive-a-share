294a642634893415c13ee7a652186a0ab639cf87
Russell Bentulan <russellbentulan@gmail.com>
2019-10-13 00:23:30 -0400

    refactored autocomplete to handle both location inputs

--- scripts.js
+++ scripts.js
@@ -4,37 +4,36 @@ dasApp = {};
 // Caching selectors
 dasApp.originInput = $('#origin-input');
 dasApp.originDatalist = $('#origin-locations');
+dasApp.destinationInput = $('#destination-input');
+dasApp.destionationDatalist = $('#destination-locations')
 
-// Get location hints from user input
-// Add datalist options to the text input based on api response
-dasApp.getHintLocations = searchTerms => {
-    $.ajax({
-        url: `${dasAppConfig.mbPlaces}${searchTerms}.json?access_token=${dasAppConfig.mbToken}&autocomplete=true&types=place&limit=5`,
-        method: 'GET',
-        dataType: 'json'
-    }).then(data => {
-        const locationObjects = data.features.values();
-        for ( const location of locationObjects ) {
-            const datalistHtml = `
-                <option value="${location.place_name}">
-            `;
-            dasApp.originDatalist.append(datalistHtml);
-        }
-    });
-}
 
 // Get value of input element
-// Assign possible hint locations to an array every 3rd letter
+// Query data every 3rd change to input
+// Add options to the datalist using the response
 dasApp.getSearchTerms = function() {
     searchTerms = $(this).val();
     if ( searchTerms.length % 3 === 0 ) {
-        dasApp.getHintLocations(searchTerms);
+        $.ajax({
+            url: `${dasAppConfig.mbPlaces}${searchTerms}.json?access_token=${dasAppConfig.mbToken}&autocomplete=true&types=place&limit=5`,
+            method: 'GET',
+            dataType: 'json'
+        }).then(data => {
+            const allLocations = data.features.values();
+            for (const location of allLocations) {
+                const optiontHtml = `<option value="${location.place_name}"`;
+                $(this).next('datalist').append(optiontHtml);
+            }
+        }).catch(err => {
+            console.log("There must have been a mistake", err);
+        });
     }
 }
 
 // init Function
 dasApp.init = () => {
     dasApp.originInput.on('input', dasApp.getSearchTerms);
+    dasApp.destinationInput.on('input', dasApp.getSearchTerms);
 };
 
 // Document Ready
