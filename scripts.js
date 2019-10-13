0c9898baec4ad6f63b034470a0508e3503e47a1d
Russell Bentulan <russellbentulan@gmail.com>
2019-10-13 16:23:14 -0400

    pulled navigation information from api

--- scripts.js
+++ scripts.js
@@ -2,11 +2,13 @@
 dasApp = {};
 
 // Caching selectors
+dasApp.tripForm = $('#trip-input');
 dasApp.originInput = $('#origin-input');
 dasApp.destinationInput = $('#destination-input');
+dasApp.switchInputsButton = $('#switch-locations');
 
 // Log searched cities with their coordinate array to save extra api requests
-dasApp.loggedLocations = {};
+dasApp.loggedCoordinates = {};
 
 // Get value of input element
 // Query data once 3 or more letters have been entered
@@ -24,7 +26,7 @@ dasApp.getSearchTerms = function() {
             for (const locationInfo of allLocations) {
                 const optiontHtml = `<option value="${locationInfo.place_name}"`;
                 $(this).next('datalist').append(optiontHtml);
-                dasApp.loggedLocation[locationInfo.place_name] = locationInfo.center;
+                dasApp.loggedCoordinates[locationInfo.place_name] = locationInfo.center;
             }
         }).catch(err => {
             console.log("There must have been a mistake", err);
@@ -32,10 +34,43 @@ dasApp.getSearchTerms = function() {
     }
 }
 
+// Take origin and destination coordinates
+dasApp.getNavigationInfo = coordinatesObject => {
+    const coordinatesString = coordinatesObject.orgin[0] + "," + coordinatesObject.orgin[1] + ";" + coordinatesObject.destination[0] + "," + coordinatesObject.destination[1];
+    
+    $.ajax({
+        url: `${dasAppConfig.mbDirections}${coordinatesString}.json?access_token=${dasAppConfig.mbToken}`,
+        method: 'GET',
+        dataType: 'json'
+    }).then(navigationObject => {
+        const navInfo = navigationObject.routes[0];
+        console.log(navInfo.distance, navInfo.duration);
+    }).catch(err => {
+        console.log("There must have been a mistake", err);
+    });
+};
+
+// Get origin and destination values and match them to their coordinates
+// Query the API for the navigation instructions
+dasApp.getFormValues = () => {
+    const originName = dasApp.originInput.val();
+    const destinationName = dasApp.destinationInput.val();
+    const coordinates = {
+        orgin: dasApp.loggedCoordinates[originName],
+        destination: dasApp.loggedCoordinates[destinationName]
+    };
+    const navigationInfo = dasApp.getNavigationInfo(coordinates);
+}
+
 // init Function
 dasApp.init = () => {
-    dasApp.originInput.on('input', dasApp.getSearchTerms);
-    dasApp.destinationInput.on('input', dasApp.getSearchTerms);
+    dasApp.tripForm.on('submit', e => {
+        e.preventDefault();
+        dasApp.getFormValues();
+    });
+    dasApp.originInput.on('keyup', dasApp.getSearchTerms);
+    dasApp.destinationInput.on('keyup', dasApp.getSearchTerms);
+    dasApp.switchInputsButton.on('input', e => console.log(e));    
 };
 
 // Document Ready
